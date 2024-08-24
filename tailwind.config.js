1887ec7bd840b32fdae7d0c39d37817efa518e2c
lucashsu95 <lucashsu95@gmail.com>
2024-08-24 16:34:46 +0800

    Update taiwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
-import colors from 'tailwindcss/colors'
 export const content = ['./src/**/*.{vue,js,ts,jsx,tsx}']
 export const plugins = []
 export const theme = {
-  colors: {
-    ...colors,
-    primary: '#63e2b7',
-    warning: '#f2c97d',
-    danger: '#e88080',
-    info: '#70c0e8'
+  extend: {
+    colors: {
+      primary: '#63e2b7',
+      warning: '#f2c97d',
+      danger: '#e88080',
+      info: '#70c0e8'
+    }
   }
 }
